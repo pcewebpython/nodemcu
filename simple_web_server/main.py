5103c718c0d2474cbb9b165d836147a624ff1ff1
Joseph Schilz <joseph@schilz.org>
2018-08-18 13:17:12 -0700

    Add simple web server.

--- simple_web_server/main.py
+++ simple_web_server/main.py
@@ -3,25 +3,13 @@ try:
 except:
     import socket
 
-response_404 = """HTTP/1.0 404 NOT FOUND
-
-<h1>404 Not Found</h1>
-"""
-
-response_500 = """HTTP/1.0 500 INTERNAL SERVER ERROR
-
-<h1>500 Internal Server Error</h1>
-"""
-
 response_template = """HTTP/1.0 200 OK
 
 %s
 """
-
 import machine
 import ntptime, utime
 from machine import RTC
-from time import sleep
 
 rtc = RTC()
 try:
@@ -41,16 +29,6 @@ def time():
 
     return response_template % body
 
-def dummy():
-    body = "This is a dummy endpoint"
-
-    return response_template % body
-
-handlers = {
-    'time': time,
-    'dummy': dummy,
-}
-
 def main():
     s = socket.socket()
     ai = socket.getaddrinfo("0.0.0.0", 8080)
@@ -60,10 +38,10 @@ def main():
 
     s.bind(addr)
     s.listen(5)
-    print("Listening, connect your browser to http://<this_host>:8080")
+    print("Listening, connect your browser to http://<this_host>:8080/")
 
     while True:
-        sleep(.1)
+        sleep(.5)
         res = s.accept()
         client_s = res[0]
         client_addr = res[1]
@@ -71,15 +49,7 @@ def main():
         print("Request:")
         print(req)
 
-        try:
-            path = req.decode().split("\r\n")[0].split(" ")[1]
-            handler = handlers[path.strip('/').split('/')[0]]
-            response = handler()
-        except KeyError:
-            response = response_404
-        except Exception as e:
-            response = response_500
-            print(str(e))
+        response = time()
 
         client_s.send(b"\r\n".join([line.encode() for line in response.split("\n")]))
 

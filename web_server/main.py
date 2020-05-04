cbff76443356c9d19b5238b55be03882cc00abd1
ajaggarcia10 <45190522+ajaggarcia10@users.noreply.github.com>
2020-05-03 18:15:10 -0700

    Finished IoT Assignment

--- web_server/main.py
+++ web_server/main.py
@@ -8,28 +8,27 @@ response_404 = """HTTP/1.0 404 NOT FOUND
 <h1>404 Not Found</h1>
 """
 
-response_500 = """HTTP/1.0 500 INTERNAL SERVER ERROR
-
+response_500 = """HTTP/1.0 500 INTERNAL ERROR
 <h1>500 Internal Server Error</h1>
 """
+
+
 
 response_template = """HTTP/1.0 200 OK
 
 %s
 """
-
 import machine
 import ntptime, utime
 from machine import RTC
-from time import sleep
 
+rtc = RTC()
 try:
     seconds = ntptime.time()
 except:
     seconds = 0
-
-rtc = RTC()
 rtc.datetime(utime.localtime(seconds))
+
 
 def time():
     body = """<html>
@@ -42,17 +41,50 @@ def time():
 
     return response_template % body
 
+
 def dummy():
     body = "This is a dummy endpoint"
 
     return response_template % body
 
-pin = machine.Pin(10, machine.Pin.IN)
+
+led = machine.Pin(9, machine.Pin.OUT)
+
+
+def light_on():
+    led.value(1)
+    body = "You turned the light on!"
+    return response_template % body
+
+
+def light_off():
+    led.value(0)
+    body = 'You turned the light off!'
+    return response_template % body
+
+
+switch_pin = machine.Pin(10, machine.Pin.IN)
+
+
+def switch():
+    body = "{state: " + str(switch_pin.value()) + "}"
+    return response_template % body
+
+adc = machine.ADC(0)
+
+def light_level():
+     body = "{value: " + str(adc.read()) + "}"
+     return response_template % body
 
 handlers = {
     'time': time,
     'dummy': dummy,
+    'light_on': light_on,
+    'light_off': light_off,
+    'switch': switch,
+    'light_level': light_level,
 }
+
 
 def main():
     s = socket.socket()
@@ -63,7 +95,7 @@ def main():
 
     s.bind(addr)
     s.listen(5)
-    print("Listening, connect your browser to http://<this_host>:8080")
+    print("Listening, connect your browser to http://<this_host>:8080/")
 
     while True:
         res = s.accept()

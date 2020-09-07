a066f2b4ee49b5db30d8e68de1183b9f30af1570
matt.gummel <matt.gummel@sap.com>
2020-09-06 22:14:50 -0700

    full webserver functionality

--- flexible_web_server/main.py
+++ flexible_web_server/main.py
@@ -3,53 +3,71 @@ try:
 except:
     import socket
 
-response_404 = """HTTP/1.0 404 NOT FOUND
-
-<h1>404 Not Found</h1>
-"""
-
-response_500 = """HTTP/1.0 500 INTERNAL SERVER ERROR
-
-<h1>500 Internal Server Error</h1>
-"""
-
 response_template = """HTTP/1.0 200 OK
 
 %s
 """
-
 import machine
 import ntptime, utime
 from machine import RTC
-from time import sleep
-
+seconds = ntptime.time()
 rtc = RTC()
-try:
-    seconds = ntptime.time()
-except:
-    seconds = 0
 rtc.datetime(utime.localtime(seconds))
+
+adc = machine.ADC(0)
+pin = machine.Pin(16, machine.Pin.OUT)
+switch_pin = machine.Pin(10, machine.Pin.IN)
+
 
 def time():
     body = """<html>
-<body>
-<h1>Time</h1>
-<p>%s</p>
-</body>
-</html>
-""" % str(rtc.datetime())
+ <body>
+ <h1>Time</h1>
+ <p>%s</p>
+ </body>
+ </html>
+    """ % str(rtc.datetime())
 
     return response_template % body
+
 
 def dummy():
     body = "This is a dummy endpoint"
 
     return response_template % body
 
+
+def light():
+    body = "{value: " + adc.read() + "}"
+    return response_template % body
+
+
+def switch():
+    body = "{state: " + switch_pin.value() + "}"
+    return response_template % body
+
+
+def light_on():
+     pin.value(0)
+     body = "You turned a light on!"
+     return response_template % body
+
+
+def light_off():
+     pin.value(1)
+     body = "You turned a light off!"
+     return response_template % body
+
+
 handlers = {
     'time': time,
     'dummy': dummy,
+    'light_on': light_on,
+    'light_off': light_off,
+    'light': light,
+    'switch': switch,
 }
+
 
 def main():
     s = socket.socket()
@@ -60,10 +78,9 @@ def main():
 
     s.bind(addr)
     s.listen(5)
-    print("Listening, connect your browser to http://<this_host>:8080")
+    print("Listening, connect your browser to http://<this_host>:8080/")
 
     while True:
-        sleep(1)
         res = s.accept()
         client_s = res[0]
         client_addr = res[1]
@@ -71,16 +88,18 @@ def main():
         print("Request:")
         print(req)
 
-        try:
-            path = req.decode().split("\r\n")[0].split(" ")[1]
-            handler = handlers[path.strip('/').split('/')[0]]
-            response = handler()
-        except KeyError:
-            response = response_404
-        except Exception as e:
-            response = response_500
-            print(str(e))
+        # The first line of a request looks like "GET /arbitrary/path/ HTTP/1.1".
+        # This grabs that first line and whittles it down to just "/arbitrary/path/"
+        path = req.decode().split("\r\n")[0].split(" ")[1]
 
+        # Given the path, identify the correct handler to use
+        handler = handlers[path.strip('/').split('/')[0]]
+
+        response = handler()
+
+        # A handler returns an entire response in the form of a multi-line string.
+        # This breaks up the response into single strings, byte-encodes them, and
+        # joins them back together with b"\r\n". Then it sends that to the client.
         client_s.send(b"\r\n".join([line.encode() for line in response.split("\n")]))
 
         client_s.close()
